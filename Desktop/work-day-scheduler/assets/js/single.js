64dfb99f2403237fa3eed8c638dfbaf3982bd524
Keegam Jervis <viakeegan@gmail.com>
2022-01-04 22:52:39 -0500

    add single js file

---
+++ Desktop/work-day-scheduler/assets/js/single.js
@@ -0,0 +1,88 @@
+var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoIssues = function(repo) {
+  var apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
+
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        // pass response data to dom function
+        displayIssues(data)
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
+      });
+    }
+    else {
+      // if not succesful, redirect to homepage
+      document.location.replace("./index.html");
+    }
+  });
+};
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  }
+  else {
+    document.location.replace("./index.html");
+  }
+};
+
+var displayIssues = function(issues) {
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!"
+    return;
+  }
+  for (var i = 0; i < issues.length; i++) {
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    // create span to hold issue title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+
+    // append to container
+    issueEl.appendChild(titleEl);
+
+    // create a type element
+    var typeEl = document.createElement("span");
+
+    // check if issue is an actual issue or pull request
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    } else {
+      typeEl.textContent = "(Issue)";
+    }
+
+    // append to container
+    issueEl.appendChild(typeEl);
+
+    issueContainerEl.appendChild(issueEl);
+  }
+};
+
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", `https://github.com/${repo}/issues`);
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
